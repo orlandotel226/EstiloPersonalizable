6006c1a8deabe51f72bea773e8cb22db5ba0804c
Orlando Cibrián <orlcibrrey12345@gmail.com>
2020-11-27 18:59:11 -0600

    Archivos de estilos con JQuery

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$("body").css("background", "#ABE6FF");
+
+$("h1").css("text-align", "center");
+
+$("#welcome_message").css({
+
+	"position" : "relative",
+	"left" : "100px",
+	"top" : "100px",
+	"font-family" : "Rubik, sans-serif"
+
+});
+
+$("#text_agreement").css({
+
+	"position" : "relative",
+	"top" : "370px",
+	"text-align" : "center",
+	"font-family" : "Rubik, sans-serif"
+
+});
+
+$("#owner_company").css({
+
+	"position" : "relative",
+	"top" : "380px",
+	"text-align" : "center",
+	"font-family" : "Rubik, sans-serif"
+
+});
